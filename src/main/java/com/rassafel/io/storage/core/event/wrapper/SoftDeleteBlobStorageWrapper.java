51623211052fd99f10b46f6cc3a72e6b6d46be0b
Sergey Koroblyov <koroblyov.sa@gmail.com>
2024-04-30 20:43:12 +0400

    Add SoftDeleteBlobStorageWrapper.

---
+++ src/main/java/com/rassafel/io/storage/core/event/wrapper/SoftDeleteBlobStorageWrapper.java
@@ -0,0 +1,141 @@
+package com.rassafel.io.storage.core.event.wrapper;
+
+import com.rassafel.commons.util.Assert;
+import com.rassafel.io.storage.core.BlobStorage;
+import com.rassafel.io.storage.core.NotFoundBlobException;
+import com.rassafel.io.storage.core.StoredBlobObject;
+import com.rassafel.io.storage.core.event.BlobEventPublisher;
+import com.rassafel.io.storage.core.event.type.RestoreSoftDeletedBlobEvent;
+import com.rassafel.io.storage.core.event.type.SoftDeleteBlobEvent;
+import com.rassafel.io.storage.core.query.StoreBlobRequest;
+import com.rassafel.io.storage.core.query.StoreBlobResponse;
+import com.rassafel.io.storage.core.query.UpdateAttributesRequest;
+import com.rassafel.io.storage.core.query.UpdateAttributesResponse;
+import com.rassafel.io.storage.core.query.impl.DefaultUpdateAttributesRequest;
+import lombok.val;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.lang.Nullable;
+
+import java.io.InputStream;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+/**
+ * Soft delete blob storage wrapper.
+ * <p>
+ * Events:
+ * <li>{@link SoftDeleteBlobEvent}</li>
+ * <li>{@link RestoreSoftDeletedBlobEvent}</li>
+ */
+public class SoftDeleteBlobStorageWrapper implements BlobStorage {
+    public static final String DEFAULT_DELETED_ATTRIBUTE = "X-Deleted";
+    private final BlobStorage delegate;
+    private final Clock clock;
+    private final BlobEventPublisher publisher;
+    private final String deletedAttribute;
+
+    public SoftDeleteBlobStorageWrapper(BlobStorage delegate, Clock clock, BlobEventPublisher publisher) {
+        this(delegate, clock, publisher, DEFAULT_DELETED_ATTRIBUTE);
+    }
+
+    public SoftDeleteBlobStorageWrapper(BlobStorage delegate, Clock clock, BlobEventPublisher publisher, String deletedAttribute) {
+        Assert.notNull(delegate, "delegate cannot be null");
+        Assert.notNull(clock, "clock cannot be null");
+        Assert.notNull(publisher, "publisher cannot be null");
+        Assert.hasText(deletedAttribute, "deletedAttribute cannot be null");
+        this.delegate = delegate;
+        this.clock = clock;
+        this.publisher = publisher;
+        this.deletedAttribute = deletedAttribute;
+    }
+
+    @Override
+    public StoreBlobResponse store(InputStream inputStream, StoreBlobRequest request) {
+        return delegate.store(inputStream, request);
+    }
+
+    @Override
+    public boolean existsByRef(String ref) {
+        return findByRef(ref).isPresent();
+    }
+
+    public boolean existsByRefIgnoreDeleted(String ref) {
+        return delegate.existsByRef(ref);
+    }
+
+    @Override
+    public StoredBlobObject getByRef(String ref) {
+        val blob = getByRefIgnoreDeleted(ref);
+        if (blob == null || isDeleted(blob)) return null;
+        return blob;
+    }
+
+    @Nullable
+    public StoredBlobObject getByRefIgnoreDeleted(String ref) {
+        return delegate.getByRef(ref);
+    }
+
+    @Override
+    public Optional<StoredBlobObject> findByRef(String ref) {
+        return findByRefIgnoreDeleted(ref)
+            .filter(Predicate.not(this::isDeleted));
+    }
+
+    public Optional<StoredBlobObject> findByRefIgnoreDeleted(String ref) {
+        return delegate.findByRef(ref);
+    }
+
+    protected boolean isDeleted(StoredBlobObject blob) {
+        return StringUtils.isNotBlank(blob.getAttribute(deletedAttribute));
+    }
+
+    @Override
+    public boolean deleteByRef(String ref) {
+        if (!existsByRef(ref)) return false;
+        val timestamp = LocalDateTime.now(clock);
+        val request = DefaultUpdateAttributesRequest.builder()
+            .attribute(deletedAttribute, timestamp.toString())
+            .build();
+        val response = updateByRefIgnoreDeleted(ref, request);
+        val event = new SoftDeleteBlobEvent(delegate, timestamp, ref);
+        publisher.publish(event);
+        return true;
+    }
+
+    /**
+     * Restore soft deleted blob
+     *
+     * @param ref blob reference
+     * @return false if blob is not soft deleted, true if soft deleted
+     * @throws NotFoundBlobException if not exists
+     */
+    public boolean restoreDeletedByRef(String ref) {
+        val blob = findByRefIgnoreDeleted(ref)
+            .orElseThrow(() -> new NotFoundBlobException(ref));
+        if (!isDeleted(blob)) return false;
+        val request = DefaultUpdateAttributesRequest.builder()
+            .removeAttribute(deletedAttribute)
+            .build();
+        val response = updateByRefIgnoreDeleted(ref, request);
+        val event = new RestoreSoftDeletedBlobEvent(delegate, clock, ref);
+        publisher.publish(event);
+        return true;
+
+    }
+
+    public boolean hardDeleteByRef(String ref) {
+        return delegate.deleteByRef(ref);
+    }
+
+    @Override
+    public UpdateAttributesResponse updateByRef(String ref, UpdateAttributesRequest request) {
+        if (!existsByRef(ref)) throw new NotFoundBlobException(ref);
+        return updateByRefIgnoreDeleted(ref, request);
+    }
+
+    public UpdateAttributesResponse updateByRefIgnoreDeleted(String ref, UpdateAttributesRequest request) {
+        return delegate.updateByRef(ref, request);
+    }
+}
